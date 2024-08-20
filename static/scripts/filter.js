22ae12eb552622e6fcd1b7c4763cdc09e860373a
Ayoub RABYA <rabiyaeayoub@gmail.com>
2024-08-20 09:45:48 +0100

    add

--- static/scripts/filter.js
+++ static/scripts/filter.js
@@ -19,10 +19,19 @@ const creationRange = document.querySelector(".slider .progress")
 const creationResultRange = document.querySelector(".filter-item-holder .filter-result")
 const gap = 3
 
-const rangeLoad = (rangeInputs, range, resultRange) => {
-    console.log("Hi I am here")
+const rangeFunc = (e, rangeInputs, resultRange, range, gap) => {
     let minValue = parseInt(rangeInputs[0].value),
     maxValue = parseInt(rangeInputs[1].value)
+
+    if (e && (maxValue - minValue) < gap) {
+        if (e.target.classList.contains("range-min")) {
+            rangeInputs[0].value = maxValue - gap
+            minValue = parseInt(rangeInputs[0].value)
+        } else {
+            rangeInputs[1].value = minValue + gap
+            maxValue = parseInt(rangeInputs[1].value)
+        }
+    } 
 
     resultRange.textContent = `${minValue} - ${maxValue}`
 
@@ -34,33 +43,8 @@ const rangeLoad = (rangeInputs, range, resultRange) => {
     range.style.right = 100-maxGabValue+"%"
 }
 
-rangeLoad(creationRangeInputs, creationRange, creationResultRange)
-
-const rangeFunc = (e, rangeInputs, resultRange, range, gap) => {
-    let minValue = parseInt(rangeInputs[0].value),
-    maxValue = parseInt(rangeInputs[1].value)
-
-    if ((maxValue - minValue) < gap) {
-        if (e.target.classList.contains("range-min")) {
-            rangeInputs[0].value = maxValue - gap
-        } else {
-            rangeInputs[1].value = minValue + gap
-        }
-    } else {
-        resultRange.textContent = `${minValue} - ${maxValue}`
-
-        const gapValue = rangeInputs[0].max - rangeInputs[0].min
-        const minGabValue = ((rangeInputs[0].max - rangeInputs[0].value) / gapValue) * 100
-        const maxGabValue = 100-((rangeInputs[0].max - rangeInputs[1].value) / gapValue) *100
-
-        range.style.left = 100-minGabValue+"%"
-        range.style.right = 100-maxGabValue+"%"
-
-    }
-    
-    return {minValue, maxValue}
-}
+rangeFunc(null, creationRangeInputs, creationResultRange, creationRange, gap)
 
 creationRangeInputs.forEach(input => {
-    input.addEventListener("input", (e) => {console.log(rangeFunc(e, creationRangeInputs, creationResultRange, creationRange, gap))})
+    input.addEventListener("input", (e) => {rangeFunc(e, creationRangeInputs, creationResultRange, creationRange, gap)})
 });
