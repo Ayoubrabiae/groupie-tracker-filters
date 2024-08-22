84369e03a52eb69788ab69f7203b0dc8783062c8
Ayoub RABYA <rabiyaeayoub@gmail.com>
2024-08-22 11:45:13 +0100

    fix error handler

--- handlers/staticHandler.go
+++ handlers/staticHandler.go
@@ -3,13 +3,14 @@ package handlers
 import (
 	"net/http"
 	"os"
+	"strings"
 )
 
 func StaticHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, err := os.Open("./static/styles/master")
+		_, err := os.Open("./static" + r.URL.Path)
 
-		if os.IsNotExist(err) {
+		if os.IsNotExist(err) || strings.HasSuffix(r.URL.Path, "/") {
 			ErrorHandler(w, "Page Not Found", http.StatusNotFound)
 			return
 		}
