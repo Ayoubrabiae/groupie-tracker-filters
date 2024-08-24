17378215c27987b06d9d320d00e8b0f991397e81
Ayoub RABYA <rabiyaeayoub@gmai.com>
2024-08-24 10:54:20 +0100

    fix back button to redirect you to the last history

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -171,7 +171,7 @@ const cardsExitBtn = document.querySelector(".profile #exit")
 
 if (cardsExitBtn) {
     cardsExitBtn.addEventListener("click", () => {
-        if (document.referrer != window.location.origin+"/") {
+        if (document.referrer.split("?")[0] != window.location.origin+"/") {
            window.location.assign(window.location.origin)
             return
         }
